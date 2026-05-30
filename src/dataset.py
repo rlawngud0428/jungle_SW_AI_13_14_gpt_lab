d2b5c498f238053899d66397babf01bff12e0e91
백승진 <vromotion@gmail.com>
2026-05-30 17:31:36 +0900

    feat(dataset.py): __len__() 구현

--- src/dataset.py
+++ src/dataset.py
@@ -38,7 +38,7 @@ class GPTDataset(Dataset):
 
     def __len__(self) -> int:
         """TODO: 전체 샘플 개수를 반환합니다."""
-        raise NotImplementedError("GPTDataset.__len__을 구현하세요.")
+        return self._length
 
     def __getitem__(self, idx: int) -> tuple[torch.Tensor, torch.Tensor]:
         """
