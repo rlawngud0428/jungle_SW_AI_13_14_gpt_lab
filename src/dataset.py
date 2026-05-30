39d0e82e6cef06a99a531f3b4725592e4b891005
rlawngud0428 <kjh000428@gmail.com>
2026-05-30 16:27:29 +0900

    feat: dataset 구현 AI를 활용해서 탑다운 방식으로 학습함.

--- src/dataset.py
+++ src/dataset.py
@@ -24,12 +24,14 @@ class GPTDataset(Dataset):
         self.context_length = context_length
         self.stride = stride if stride is not None else context_length
         # TODO: 만들 수 있는 학습 샘플 개수를 self._length에 저장하세요.
-        raise NotImplementedError("GPTDataset.__init__에서 self._length를 구현하세요.")
+        # raise NotImplementedError("GPTDataset.__init__에서 self._length를 구현하세요.")
+        self._length = (len(self.token_ids) - self.context_length - 1) // self.stride + 1
 
     def __len__(self) -> int:
         """TODO: 전체 샘플 개수를 반환합니다."""
-        raise NotImplementedError("GPTDataset.__len__을 구현하세요.")
-
+        # raise NotImplementedError("GPTDataset.__len__을 구현하세요.")
+        return self._length
+    
     def __getitem__(self, idx: int) -> tuple[torch.Tensor, torch.Tensor]:
         """
         TODO: idx번째 input_ids와 target_ids를 LongTensor로 반환합니다.
@@ -38,7 +40,17 @@ class GPTDataset(Dataset):
             input_ids: (context_length,)
             target_ids: (context_length,)
         """
-        raise NotImplementedError("GPTDataset.__getitem__을 구현하세요.")
+        # raise NotImplementedError("GPTDataset.__getitem__을 구현하세요.")
+        start = idx * self.stride
+        end = start + self.context_length
+
+        input_ids = self.token_ids[start : end]
+        target_ids = self.token_ids[start + 1 : end + 1]
+        
+        return (
+            torch.tensor(input_ids, dtype=torch.long),
+            torch.tensor(target_ids, dtype=torch.long)
+        )
 
 
 def create_dataloader(
@@ -51,4 +63,19 @@ def create_dataloader(
     num_workers: int = 0,
 ) -> DataLoader:
     """TODO: GPTDataset을 만들고 torch.utils.data.DataLoader로 감싸 반환합니다."""
-    raise NotImplementedError("create_dataloader를 구현하세요.")
+    # raise NotImplementedError("create_dataloader를 구현하세요.")
+    dataSet= GPTDataset(
+        token_ids=token_ids,
+        context_length=context_length,
+        stride=stride
+    )
+
+    dataloader = DataLoader(
+        dataSet,
+        batch_size=batch_size,
+        shuffle=shuffle,
+        drop_last=drop_last,
+        num_workers=num_workers
+    )
+
+    return dataloader
