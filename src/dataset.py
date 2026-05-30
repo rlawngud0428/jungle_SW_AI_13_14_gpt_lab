fba82e084eb557ba259a5ae99addab3806bd7d2e
백승진 <vromotion@gmail.com>
2026-05-30 17:49:24 +0900

    feat(dataset.py): create_dataloader() 함수 구현
    사전 정의된 함수 시그니처에 따라 클래스 인자 정의

--- src/dataset.py
+++ src/dataset.py
@@ -71,4 +71,11 @@ def create_dataloader(
     num_workers: int = 0,
 ) -> DataLoader:
     """TODO: GPTDataset을 만들고 torch.utils.data.DataLoader로 감싸 반환합니다."""
-    raise NotImplementedError("create_dataloader를 구현하세요.")
+    dataset = GPTDataset(token_ids, context_length, stride=stride)
+    return DataLoader(
+        dataset,
+        batch_size=batch_size,
+        shuffle=shuffle,
+        drop_last=drop_last,
+        num_workers=num_workers,
+    )
