120f9d2a6bf976b4036250282726e0df0da95eba
백승진 <vromotion@gmail.com>
2026-06-01 16:47:15 +0900

    feat(model.py): LayerNorm 클래스 구현

--- src/model.py
+++ src/model.py
@@ -23,7 +23,10 @@ class LayerNorm(nn.Module):
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """TODO: 마지막 차원의 평균과 분산으로 정규화한 뒤 gamma/beta를 적용합니다."""
-        raise NotImplementedError("LayerNorm.forward를 구현하세요.")
+        mean = x.mean(dim=-1, keepdim=True)
+        var = x.var(dim=-1, keepdim=True, unbiased=False)
+        x_norm = (x - mean) / torch.sqrt(var + self.eps)
+        return self.gamma * x_norm + self.beta
 
 
 class GELU(nn.Module):
