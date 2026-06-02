bc792629a8cacd43566cfbde049833b7207384b0
백승진 <vromotion@gmail.com>
2026-06-02 10:44:55 +0900

    feat(model.py): GPTModel 클래스 구현

--- src/model.py
+++ src/model.py
@@ -104,7 +104,25 @@ class GPTModel(nn.Module):
         super().__init__()
         self.config = config
         # TODO: embedding, blocks, final layernorm, lm_head를 정의하세요.
-        raise NotImplementedError("GPTModel.__init__을 구현하세요.")
+        self.embedding = InputEmbedding(
+            vocab_size=config["vocab_size"],
+            emb_dim=config["emb_dim"],
+            context_length=config["context_length"],
+            drop_rate=config["drop_rate"],
+        )
+        self.blocks = nn.Sequential(
+            *[
+                TransformerBlock(
+                    d_model=config["emb_dim"],
+                    n_heads=config["n_heads"],
+                    drop_rate=config["drop_rate"],
+                    qkv_bias=config["qkv_bias"],
+                )
+                for _ in range(config["n_layers"])
+            ]
+        )
+        self.final_norm = LayerNorm(config["emb_dim"])
+        self.lm_head = nn.Linear(config["emb_dim"], config["vocab_size"])
 
     def forward(
         self,
@@ -118,7 +136,19 @@ class GPTModel(nn.Module):
             targets가 None이면 logits
             targets가 있으면 (loss, logits)
         """
-        raise NotImplementedError("GPTModel.forward를 구현하세요.")
+        x = self.embedding(idx)
+        x = self.blocks(x)
+        x = self.final_norm(x)
+        logits = self.lm_head(x)
+
+        if targets is None:
+            return logits
+
+        loss = nn.functional.cross_entropy(
+            logits.reshape(-1, logits.size(-1)),
+            targets.reshape(-1),
+        )
+        return loss, logits
 
 
 def generate_text_simple(
